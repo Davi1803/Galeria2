2ec0af62e75b02a08cc9a5ba8a812fbe427fa5d7
20201tiimi0047 <sallescardosodavi@gmail.com>
2023-05-19 09:06:38 -0300

    iniciando

--- app/src/main/java/salles/cardoso/davi/galeria2/PhotoActivity.java
+++ app/src/main/java/salles/cardoso/davi/galeria2/PhotoActivity.java
@@ -3,16 +3,12 @@ package salles.cardoso.davi.galeria2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+public class PhotoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Toolbar toolbar = findViewById(R.id.tbMain);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_photo);
     }
 }
